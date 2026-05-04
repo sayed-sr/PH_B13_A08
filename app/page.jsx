5e285bd0f0bd73664f4c20ae4a4f6051a9a43d01
sayed <mdsayed.programmer@gmail.com>
2026-05-04 21:24:28 +0600

    spiner

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,26 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import Hero from "./components/home/Hero"
 import PopularCourses from "./components/home/PopularCourses"
 import LearningTips from "./components/home/LearningTips"
 import TopInstructors from "./components/home/TopInstructors"
 import TrendingCourses from "./components/home/TrendingCourses"
+import Spinner from "./components/Spinner"
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 800) // simulated loading
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) return <Spinner />
+
   return (
     <main className="space-y-20 pb-20">
       <Hero />
